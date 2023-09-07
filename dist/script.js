6cdb540930be742351aebc5a5f827c4fe31782ea
Jakob <shavinski.jakob@gmail.com>
2023-09-06 17:56:06 -0700

    Intial files
    Set up beginning of project
    add header, add book button, add demo books

---
+++ dist/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+
+const MY_LIBRARY = [
+    { title: "test", author: "test author", pages: 652, read: "Not read" },
+    { title: "test 2", author: "test author 2", pages: 544, read: "Not read" },
+    { title: "test 3", author: "test author 3", pages: 234, read: "Completed book" },
+];
+
+function Book(title, author, pages, read) {
+    // the constructor...
+
+}
+
+function addBookToLibrary() {
+    // do stuff here
+    const booksContainer = document.getElementById("books-container");
+
+    for (let i = 0; i < MY_LIBRARY.length; i++) {
+        const bookCard = document.createElement('div');
+        bookCard.className = "book-card flex flex-col justify-center items-center rounded-lg mt-5 bg-green-200 w-80 h-44";
+
+        const bookTitle = document.createElement('h2');
+        bookTitle.textContent = MY_LIBRARY[i].title;
+        bookCard.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement('h2');
+        bookAuthor.textContent = MY_LIBRARY[i].author;
+        bookCard.appendChild(bookAuthor);
+
+        const bookPages = document.createElement('h3');
+        bookPages.textContent = MY_LIBRARY[i].pages;
+        bookCard.appendChild(bookPages);
+
+        const bookRead = document.createElement('h3');
+        bookRead.textContent = MY_LIBRARY[i].read;
+        bookCard.appendChild(bookRead);
+
+        booksContainer.appendChild(bookCard);
+    }
+}
+
+console.log('my lib', MY_LIBRARY);
+
+addBookToLibrary();
+
+
